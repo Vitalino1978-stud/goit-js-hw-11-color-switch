0d8281ef545bcc82e9abf2d0d706ff83283d058b
Vitalii Susulovskyi <vitalinos1978@gmail.com>
2021-02-28 10:11:12 +0200

    adds task

--- src/index.js
+++ src/index.js
@@ -7,9 +7,30 @@ const colors = [
   '#795548',
 ];
 
-const bodyRef = document.querySelector('body');
-const startbtnRef = document.querySelector('selector');
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[data-action="stop"]')
+}
+let timerId = null;
+refs.startBtn.addEventListener('click', onStart);
+refs.stopBtn.addEventListener('click', onStop);
 
-const randomIntegerFromInterval = (min, max) => {
+function onStart() {
+
+  timerId = setInterval(() => {
+    const currentColorIndex = randomIntegerFromInterval(0, colors.length - 1);
+    refs.body.style.backgroundColor = colors[currentColorIndex]
+  }, 1000);
+  refs.startBtn.disabled = true;
+  
+};
+
+function onStop() {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+};
+
+function randomIntegerFromInterval (min, max)  {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
